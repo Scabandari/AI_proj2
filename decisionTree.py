0aa4b271293cb3832b29c7897ac97cc10af72760
jy <jennyliu1993@hotmail.com>
2018-11-12 23:02:01 -0500

    decision tree can now run combinations of hyperparameters to find the most suitable ones, export the best model, and make prediction using existing model and test data didnt do k fold test result is still of very low accuracy

--- decisionTree.py
+++ decisionTree.py
@@ -1,19 +1,118 @@
 from sklearn import tree
 from sklearn.metrics import accuracy_score
+import csv
+import numpy as np
+import itertools
+import pickle
 
 
-def decision_tree(samples, features, test_samples, test_features, description):
+def load_csv(train_data_filename, validation_data_filename):
+    pass
+    with open(train_data_filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+    data = []
+    for row in reader:
+        # print(row)
+        new_row = [int(number) for number in row]
+        data.append(new_row)
+    file.close()
+    train_features = [d[:-1] for d in data]
+    train_labels = [d[-1] for d in data]
 
-    clf = tree.DecisionTreeClassifier()
-    clf.fit(samples, features)
+    with open(validation_data_filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        data = []
+        for row in reader:
+            new_row = [int(number) for number in row]
+            data.append(new_row)
+    file.close()
 
-    prediction = clf.predict(test_samples)
+    test_features = [d[:-1] for d in data]
+    test_labels = [d[-1] for d in data]
 
-    accuracy = accuracy_score(test_features, prediction)
+    return train_features, train_labels, test_features, test_labels
 
-    return {
-        'description': description,
-        'score': accuracy
+
+def decision_tree(features, labels, test_features, test_labels):
+    DECISION_TREE_ACCURACIES = {
+        'Accuracy': 0
     }
 
+    # source: https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+    CRITERION = ['gini', 'entropy']
+    SPLITTER = ['best', 'random']
+    MAX_DEPTH = list(np.arange(30, 60, 5))
+    MAX_DEPTH.append(None)
+    MIN_SAMPLES_SPLIT = list(np.arange(2, 10, 2))
+    # MIN_SAMPLES_SPLIT.append(2)
+    MIN_SAMPLES_LEAF = list(np.arange(1, 5))
+    MIN_IMPURITY_DECREASE = list(np.arange(0.0, 0.5, 0.1))
 
+    for criterion, splitter, max_depth, min_samples_split, min_samples_leaf, min_impurity_decrease in itertools.product(
+            CRITERION, SPLITTER, MAX_DEPTH, MIN_SAMPLES_SPLIT, MIN_SAMPLES_LEAF, MIN_IMPURITY_DECREASE):
+        classifier = tree.DecisionTreeClassifier(criterion=criterion, splitter=splitter, max_depth=max_depth,
+                                                 min_samples_split=min_samples_split,
+                                                 min_samples_leaf=min_samples_leaf, min_weight_fraction_leaf=0.0,
+                                                 max_features=None,
+                                                 random_state=None, max_leaf_nodes=None,
+                                                 min_impurity_decrease=min_impurity_decrease,
+                                                 min_impurity_split=None, class_weight=None, presort=False)
+        classifier.fit(features, labels)
+        # print('finished fitting')
+
+        validation_predicted = classifier.predict(test_features)
+        # print('finished prediction')
+        # print(validation_predicted)
+        accuracy = accuracy_score(test_labels, validation_predicted)
+        # print(accuracy)
+
+        if accuracy > DECISION_TREE_ACCURACIES['Accuracy']:
+            DECISION_TREE_ACCURACIES['Accuracy'] = accuracy
+            DECISION_TREE_ACCURACIES['Criterion'] = criterion
+            DECISION_TREE_ACCURACIES['Splitter'] = splitter
+            DECISION_TREE_ACCURACIES['Max_depth'] = max_depth
+            DECISION_TREE_ACCURACIES['Min_sample_split'] = min_samples_split
+            DECISION_TREE_ACCURACIES['Min_sample_leaf'] = min_samples_leaf
+            DECISION_TREE_ACCURACIES['Min_impurity_decrease'] = min_impurity_decrease
+
+    classifier = tree.DecisionTreeClassifier(criterion=DECISION_TREE_ACCURACIES['Criterion'],
+                                             splitter=DECISION_TREE_ACCURACIES['Splitter'],
+                                             max_depth=DECISION_TREE_ACCURACIES['Max_depth'],
+                                             min_samples_split=DECISION_TREE_ACCURACIES['Min_sample_split'],
+                                             min_samples_leaf=DECISION_TREE_ACCURACIES['Min_sample_leaf'],
+                                             min_impurity_decrease=DECISION_TREE_ACCURACIES['Min_impurity_decrease'])
+    classifier.fit(features, labels)
+    with open('DTmodel.pkl', 'wb') as file:
+        pickle.dump(classifier, file)
+
+    return DECISION_TREE_ACCURACIES
+
+
+def predict(testset_filename, model_filename):
+    pass
+    # load testset features
+    with open(testset_filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        testset_data = []
+        for row in reader:
+            # print(row)
+            new_row = [int(number) for number in row]
+            testset_data.append(new_row)
+    file.close()
+
+    # load model
+    with open(model_filename, 'rb') as modelfile:
+        classifier = pickle.load(modelfile)
+    modelfile.close()
+
+    prediction = classifier.predict(testset_data)
+    with open('Test-dt.csv', 'w') as result_file:
+        for i in range(len(prediction)):
+            result_file.write('%d,%d\n' % (i + 1, prediction[i]))
+
+
+if __name__ == "__main__":
+    # features, labels, test_features, test_labels = load_csv('ds1Train.csv', 'ds1Val.csv')
+    # dt_parameters = decision_tree(features, labels, test_features, test_labels)
+    # print(dt_parameters)
+    predict('ds1Test.csv', 'DTmodel.pkl')
