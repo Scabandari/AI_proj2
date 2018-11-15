837969554b3af3631bdd3f3a91364a42b05d6281
Scabandari <ryan3nichols@gmail.com>
2018-11-15 18:05:34 -0500

    Random forests working, now naive bayes and random forests have option of being trained on 1st 2 combined datasets

--- main.py
+++ main.py
@@ -10,184 +10,100 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from copy import deepcopy
-from utils import add_data, separate_labels, rename_cols
 from naiveBayes import bernoulli
 from randomForests import random_forests
+#from sklearn.externals import joblib
+import pickle
+from copy import deepcopy
 
-# grid search https://machinelearningmastery.com/how-to-tune-algorithm-parameters-with-scikit-learn/
-
-""" 
-We're having issues that I'm sure everyone is, our predictions are not accurate. 
-K-folds should help. We're allowed to use it? Our train and test data is seperate,
-with k-folds normally you treat it as one big group and the sectioning gets 
-automated in the algo
-"""
-
-# list of dicts for each classifier {'description': "blah blah", 'score': score}
-# description should describe hyper params as well
-results = {'naive bayes': [],
-           'trees': [],
-           'random forests': [],
-           'gradient boosting': []}
+from utils import (add_data,
+                   separate_labels,
+                   rename_cols,
+                   output_files,
+                   model_fromPickle,
+                   check_accuracy,
+                   combine_sets)
 
 
-def print_results():
-    for key in results.keys():
-        for experiment_ in results[key]:
-            print("{}: {}".format(
-                experiment_['description'],
-                experiment_['score'])
-            )
-
-
-# so we can rename the columns of this df, no idea what the previous column names mean
 columns = []
 for i in range(1025):
     columns.append(i)
 
+# read in data set 1 Train and Val and Test, rename the columns
 data_train = pd.read_csv('ds1Train.csv', delimiter=',')
 rename_cols(data_train)
 data_test = pd.read_csv('ds1Val.csv', delimiter=',')
 rename_cols(data_test)
+ds1Test = pd.read_csv('ds1Test.csv', delimiter=',')
+rename_cols(ds1Test)
 
+
+# Seperate into features + labels for Data set 1
 set1_xTrain, set1_yTrain, set1_xTest, set1_yTest = separate_labels(data_train, data_test)
 
-# # Naive Bayes
-# results['naive bayes'].append(bernoulli(
-#     set1_xTrain,
-#     set1_yTrain,
-#     set1_xTest,
-#     set1_yTest,
-#     "Naive Bayes Bernoulli"))
+feature_sets = [set1_xTrain, set1_xTest]
+label_sets = [set1_yTrain, set1_yTest]
 
-# now we can do basically the same thing again after we've augmented our training data
-# by copying all the rows making small changes in the features but keeping the labels
-# since the small changes shouldn't affect which letter the instance is most like
-# AUGMENTING DATA NOT HELPING SO FAR
-# augmented_train = add_data(data_train, 0.08, 20)
-# rename_cols(augmented_train)
-# aug_xTrain, aug_yTrain = separate_labels(augmented_train)
-# results['naive bayes'].append(bernoulli(
-#     aug_xTrain,
-#     aug_yTrain,
-#     set1_xTest,
-#     set1_yTest,
-#     "Naive Bayes Bernoulli with augmented training data"))
+set1_features = combine_sets(feature_sets)
+set1_labels = combine_sets(label_sets)
+combined_ds1 = [set1_features, set1_labels]
 
+# load previously classified models
 
-"""Results not very good. Try Random forests?"""
+files_out = [
+    # 'ds1TEST-dt',
+    # 'ds2TEST-dt',
+    'ds1TEST-nb',
+    # 'ds2TEST-nb',
+    'ds1TEST-3' #,
+    #'d2TEST-3'
+]
 
-"""Decision trees """
-accuracy = {}
-max_depths = []
-test_accuracies = []
+trained_models = []   # testing only
 
-for i in range(6, 40, 2):
-    max_depths.append(i)
-    tree = DecisionTreeClassifier(max_depth=i, random_state=0)
-    tree.fit(set1_xTrain, set1_yTrain)
-    train = tree.score(set1_xTrain, set1_yTrain)
-    test = tree.score(set1_xTest, set1_yTest)
-    test_accuracies.append(test)
-    data = {'max depth': i,
-     'train': train,
-     'test': test}
-    accuracy[i] = data
+for file_name in files_out:
+    f = deepcopy(file_name)
+    f += ".pkl"
+    model = model_fromPickle(f)
+    if model is not None:
+        trained_models.append(model)  # testing only
+        f = deepcopy(file_name)
+        f += ".csv"
+        output_files(model, ds1Test, f)
 
-for data in accuracy.keys():
-    print("\n\nMax depth: {}".format(accuracy[data]['max depth']))
-    print("Accuracy of Decision Tree classifier on training set is: {:.3f}"
-          .format(accuracy[data]['train']))
-    # above accuracy is .99 which mean overfitting
-    print("Accuracy of Decision Tree classifier on testing set is: : {:.3f}"
-          .format(accuracy[data]['test']))
-
-plt.barh(range(len(max_depths)), test_accuracies, align='center')
-plt.yticks(np.arange(len(max_depths)), max_depths)
-plt.xlabel("Accuracy")
-plt.ylabel("Max depth")
-plt.show()
+########## testing only #####################################
+for index, model in enumerate(trained_models):
+    accuracy = check_accuracy(model, set1_xTest, set1_yTest)
+    print("Model {}: {}".format(index, accuracy))
+##############################################################
 
 
-"""Random forests are basically a collection of decision trees, parameter ?max_forests?
-    can be changed to control the randomness of each tree"""
-# Anyhow, regardless of notation issues, the two main parameters of RF are the number of
-#  trees grown and the number of predictors randomly tried at each split.
-# https://stats.stackexchange.com/questions/218781/what-are-the-model-parameters-and-hyperparameters-of-random-forest-classifier#218967
-# hyper_params_1 = [
-#     {'n_estimators': 125},
-#     {'n_estimators': 150},
-#     {'n_estimators': 200},
-#     {'n_estimators': 300},
-#     {'n_estimators': 400},
-#     {'n_estimators': 500},
-# ]
+# create dict to store some meta-data on params and performance after tuning in case we want access
+# later for summary statistics
+best = {
+    'naive bayes': {},
+    'decision trees': {},
+    'random forests': {}
+}
 
-# hyper_params_2 = [
-#     {
-#         'n_estimators': 105,
-#         'max_features': 5
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 10
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 15
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 20
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 25
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 30
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 35
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 40
-#     },
-#     {
-#         'n_estimators': 105,
-#         'max_features': 45
-#     }
-# ]
-#
-# for hyp_par in hyper_params_2:
-#     results['random forests'].append(random_forests(
-#         set1_xTrain,
-#         set1_yTrain,
-#         set1_xTest,
-#         set1_yTest,
-#         "Random Forests with n_estimators: {} max_features{}".format(
-#             hyp_par['n_estimators'],
-#             hyp_par['max_features']),
-#         hyp_par
-#     ))
+# Get classifier and meta data on Naive Bayes for ds1
+bern, best['naive bayes'] = bernoulli(set1_xTrain,
+                                      set1_yTrain,
+                                      set1_xTest,
+                                      set1_yTest,
+                                      data_set1=True,
+                                      combined=combined_ds1)
+output_files(bern, set1_xTrain, 'ds1Test-nb.csv')  #todo should be final test set here (set1_features)
 
-# Best score found was n_estimators=150, max_features=10
+# Get classifier and meta data on Random Forests
+#rf = random_forests(set1_xTrain, set1_yTrain, set1_xTest, set1_yTest, data_set1=True)
+rf, best['random forests'] = random_forests(set1_xTrain,
+                                            set1_yTrain,
+                                            set1_xTest,
+                                            set1_yTest,
+                                            data_set1=True,
+                                            combined=combined_ds1)
+output_files(rf, set1_xTrain, 'ds1Test-3.csv')  #todo should be final test set here
 
-# still need to tune params here
-"""Gradient boosting is another ensemble classifier like Random Forests"""
-# results['gradient boosting'].append(random_forests(
-#         set1_xTrain,
-#         set1_yTrain,
-#         set1_xTest,
-#         set1_yTest,
-#         "Gradient boosting:"
-#     ))
 
-print_results()
-
-#################################################################################################
-##                    DATA SET 2 BELOW
-##############################################################################################
+####################################################################################
--- naiveBayes.py
+++ naiveBayes.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
+import itertools
+from sklearn.externals import joblib
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import accuracy_score
+import pickle
 
 """Naive bayes: https://www.youtube.com/watch?v=99MN-rl8jGY
     bernoulie ==> binary features but we can try the other 2 kinds for 
     experimentation"""
 
 
-def bernoulli(train_data, train_labels, test_data, test_labels, description):
+def bernoulli(train_data, train_labels, test_data, test_labels, data_set1=True, combined=None):
     """
     Performs the Naive Bayes Bernoulli classification on the test data after training the model
     on the training data
@@ -19,17 +22,46 @@ def bernoulli(train_data, train_labels, test_data, test_labels, description):
     :param description:
     :return:
     """
-    bern = BernoulliNB()
-    bern.fit(train_data, train_labels)
-    # ignore below for now, can find out if model is overfit by testing on training set?
-    # y_pred_train = bern.predict(set1_xTrain)
-    # acc = accuracy_score(set1_yTrain, y_pred_train)
-    # print("Accuracy score for bernouli naive bayes on training data set 1: {}".format(acc))
 
-    # now for the test set, should see lower accuracy
-    pred_test = bern.predict(test_data)
-    acc = accuracy_score(test_labels, pred_test)
-    return {
-        'description': description,
-        'score': acc
+    DECISION_TREE_ACCURACIES = {
+        'Accuracy_train': 0,
+        'Accuracy_test': 0
     }
+    ALPHA = [0, .01, .025, .05, .075, 0.1, 0.2, 0.3, .5, .75, 1, 1.5, 2.5]
+    #ALPHA = [0, 0.175, 0.190, 0.195, 0.2, 0.205, 0.21, 0.225]
+
+    FIT_PRIOR = [True, False]
+
+    for alpha, fit_prior in itertools.product(ALPHA, FIT_PRIOR):
+        bern = BernoulliNB(alpha=alpha, fit_prior=fit_prior)
+        bern.fit(train_data, train_labels)
+
+        pred_test = bern.predict(test_data)
+        acc = accuracy_score(test_labels, pred_test)
+        print("Alpha: {} Fit Prior: {} Accuracy: {}".format(alpha, fit_prior, acc))
+
+        if acc > DECISION_TREE_ACCURACIES['Accuracy_test']:
+            DECISION_TREE_ACCURACIES['Accuracy_test'] = acc  # todo this line is new, test
+            DECISION_TREE_ACCURACIES['Alpha'] = alpha
+            DECISION_TREE_ACCURACIES['Fit_prior'] = fit_prior
+            pred_train = bern.predict(train_data)
+            acc_ = accuracy_score(train_labels, pred_train)
+            DECISION_TREE_ACCURACIES['Accuracy_train'] = acc_
+
+    bern = BernoulliNB(alpha=DECISION_TREE_ACCURACIES['Alpha'],
+                       fit_prior=DECISION_TREE_ACCURACIES['Fit_prior'])
+
+    if combined is not None:
+        bern.fit(combined[0], combined[1])  # both first sets given, extra data == extra training
+    else:
+        bern.fit(train_data, train_labels)
+
+    # save the trained model
+    file_name = 'ds1TEST-nb.pkl' if data_set1 else 'ds2TEST-nb.pkl'
+    with open(file_name, 'wb') as file:
+        pickle.dump(bern, file)
+
+    return bern, DECISION_TREE_ACCURACIES
+
+
+
